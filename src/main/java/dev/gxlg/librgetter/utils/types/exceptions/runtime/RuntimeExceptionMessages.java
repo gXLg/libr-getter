a3b7c7a3ec4b220b5f5686a3915fff3a218b7e4a
Dmitry Plesunov <dima.plesunov@gmx.de>
2026-03-12 05:11:48 +0100

    refactor: add UncategorizedConfigException for better error handling

--- src/main/java/dev/gxlg/librgetter/utils/types/exceptions/runtime/RuntimeExceptionMessages.java
+++ src/main/java/dev/gxlg/librgetter/utils/types/exceptions/runtime/RuntimeExceptionMessages.java
@@ -1,7 +1,8 @@
 package dev.gxlg.librgetter.utils.types.exceptions.runtime;
 
 public enum RuntimeExceptionMessages {
-    UNEXPECTED_CONFIGURABLE("Unexpected configurable type encountered");
+    UNEXPECTED_CONFIGURABLE("Unexpected configurable type encountered"),
+    UNCATEGORIZED_CONFIG("Uncategorized config declaration");
 
     private final String message;
 
