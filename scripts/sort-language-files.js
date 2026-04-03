2dc1b6b7844631daa6d12c0b3af6317aac21a42c
Emir Oven <emirovenprivate@gmail.com>
2026-04-03 02:03:40 +0200

    Added language to language sorter

--- scripts/sort-language-files.js
+++ scripts/sort-language-files.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 const path = "./src/main/resources/assets/librgetter/lang/%.json";
-const langs = ["de_de", "en_us", "ru_ru", "ja_jp", "vi_vn", "es_es"];
+const langs = ["de_de", "en_us", "ru_ru", "ja_jp", "vi_vn", "es_es", "tr_tr"];
 
 for (const lang of langs) {
-    const fpath = path.replace("%", lang);
-    const json = JSON.parse(fs.readFileSync(fpath));
-    fs.writeFileSync(fpath, JSON.stringify(json, Object.keys(json).sort(), 2));
+  const fpath = path.replace("%", lang);
+  const json = JSON.parse(fs.readFileSync(fpath));
+  fs.writeFileSync(fpath, JSON.stringify(json, Object.keys(json).sort(), 2));
 }
