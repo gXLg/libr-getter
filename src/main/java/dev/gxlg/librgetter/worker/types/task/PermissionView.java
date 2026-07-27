d67967a47dd1b7263da095c2077fd7fc39b57046
Dmitry Plesunov <dima.plesunov@gmx.de>
2026-07-27 06:13:26 +0200

    style: fix formatting

--- src/main/java/dev/gxlg/librgetter/worker/types/task/PermissionView.java
+++ src/main/java/dev/gxlg/librgetter/worker/types/task/PermissionView.java
@@ -1,6 +1,8 @@
 package dev.gxlg.librgetter.worker.types.task;
 
-public record PermissionView(boolean allowsBreakingLecterns, boolean allowsPlacingLectern, boolean allowsSettingTradeOffers, boolean allowsOpeningScreen, boolean forcesSecondaryUse, boolean disablesBlockBreakStopping) {
+public record PermissionView(
+    boolean allowsBreakingLecterns, boolean allowsPlacingLectern, boolean allowsSettingTradeOffers, boolean allowsOpeningScreen, boolean forcesSecondaryUse, boolean disablesBlockBreakStopping
+) {
     // forcesSecondaryUse         - pretends to press the shift key to allow placing on top of clickable blocks
     // disablesBlockBreakStopping - on gameTickEnd if !attackButton.isPressed() -> stopBreakBlock() called, when we break block: prevent
     public static final PermissionView DEFAULT = new PermissionView(false, false, false, false, false, false);
